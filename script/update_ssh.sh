f308ea2ef7b8d798ed7807ce708823b34b1ca94b
liangjingyang <ljy0922@gmail.com>
2013-11-21 20:36:57 +0800

    fix bug

--- script/update_ssh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-IP=192.168.1.255
-cd /data/croods/server
-tar czf server.tar.gz config deps ebin logs script
-scp server.tar.gz $IP:/data/croods/server/
-ssh $IP "cd /data/croods/server/; pkill beam; tar xzf server.tar.gz; cd script; ./start.sh" 
